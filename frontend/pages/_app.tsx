84ab9d56c14f057a6d749b277761decb880243a8
Dai-xx <daiccixyz@gmail.com>
2024-12-13 21:05:44 +0900

    initialize frontend

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,29 @@
+import "@/styles/globals.css";
+import { Roboto } from "next/font/google";
+import type { Metadata } from "next";
+import type { AppProps } from "next/app";
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Iroha",
+  description: "",
+};
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <main className={roboto.className}>
+        <div className="bg-base-100">
+          <div className="mx-auto max-w-screen-lg px-2">
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
