76dc074c3b6b925727f3dab0755c79af89f73689
Dai-xx <daiccixyz@gmail.com>
2024-12-15 02:44:38 +0900

    Add: ページネーションの仮実装

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,38 @@
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
 import { filedataMock } from "@/mocks/filedataMock";
 import Header from "@/components/Header";
 
+const fetcher = (url: any) => axios.get(url).then((res) => res.data);
+
 export default function Home() {
   const mockData = filedataMock;
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 14;
+  const totalPages = Math.ceil(mockData.length / pageSize);
+  const currentPosts = mockData.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize,
+  );
+
+  useEffect(() => {
+    console.log("currentPage", currentPage); // currentPageの変更を監視
+  }, [currentPage]); // currentPageが変わるたびに実行される
+
+  const handlePageClick = (event: any) => {
+    setCurrentPage(event.selected + 1);
+  };
+
+  // const { data, error } = useSWR(`/api/posts?page=${currentPage}`, fetcher, {
+  //   revalidateOnFocus: false,
+  //   keepPreviousData: true,
+  // });
+
   const { setTheme, theme } = useTheme();
   const [isChecked, setIsChecked] = useState(false);
-
   useEffect(() => {
     // 現在のテーマに基づいてトグル状態を設定
     setIsChecked(theme === "light");
@@ -39,7 +64,7 @@ export default function Home() {
           <div className="mx-auto flex h-[700px] w-[1100px] flex-col items-center justify-center rounded-xl bg-base-300/50 p-5">
             <ul className="grid w-full grid-flow-col grid-cols-2 grid-rows-8 justify-items-center gap-3">
               <div className="h-[70px] w-[440px] border-collapse rounded-lg border border-dashed border-primary"></div>
-              {mockData.map((item, index) => {
+              {currentPosts.map((item, index) => {
                 return (
                   <li
                     key={index}
@@ -53,12 +78,23 @@ export default function Home() {
             </ul>
           </div>
           <div className="mt-3">
-            <nav className="join flex justify-center">
-              <button className="btn join-item">1</button>
-              <button className="btn join-item">2</button>
-              <button className="btn btn-disabled join-item">...</button>
-              <button className="btn join-item">99</button>
-              <button className="btn join-item">100</button>
+            <nav className="flex justify-center">
+              <ReactPaginate
+                previousLabel={<span className="btn join-item">Previous</span>} // DaisyUIのボタン
+                nextLabel={<span className="btn join-item">Next</span>} // DaisyUIのボタン
+                breakLabel={
+                  <span className="btn btn-disabled join-item">...</span>
+                } // "..."のボタン
+                pageCount={totalPages}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={"join flex justify-center"} // DaisyUIのjoinクラス
+                activeClassName={"btn-active"} // 現在のページのスタイル
+                pageClassName={"btn join-item"} // 各ページ番号のボタンスタイル
+                previousClassName={"btn join-item"} // 前のページのボタンスタイル
+                nextClassName={"btn join-item"} // 次のページのボタンスタイル
+              />
             </nav>
           </div>
         </div>
